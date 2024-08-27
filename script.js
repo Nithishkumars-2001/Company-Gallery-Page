8e3976513e7fb5c1bbe6253cf075f613d1118a55
Nithishkumars-2001 <nithishkumar70102@gmail.com>
2024-08-27 10:30:47 +0530

    Updated all files

---
+++ script.js
@@ -0,0 +1,32 @@
+
+       //select navbar link option
+var about_option_box = document.querySelector(".about-option-box")
+function show_about_box(){
+    about_option_box.style.display = "block"
+}
+function close_about_box(){
+    about_option_box.style.display = "none"
+}
+
+//select sidenavbar
+var sidenavbar = document.querySelector(".side-navbar")
+
+function shownavbar()
+{
+   sidenavbar.style.left = "0"
+}
+function closenavbar()
+{
+   sidenavbar.style.left = "-90%"
+}
+
+  //side navbar about option box
+
+var side_navbar_about_option_box = document.querySelector(".side-navbar-about-option-box")
+
+function show_side_navbar_about_box(){
+    side_navbar_about_option_box.style.display = "block"
+}
+function close_side_navbar_about_box(){
+    side_navbar_about_option_box.style.display = "none"
+}
